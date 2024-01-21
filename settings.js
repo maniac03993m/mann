1c6bc81a44c2832211458e5fa00995f6027c9c60
maniac03993m <157286294+maniac03993m@users.noreply.github.com>
2024-01-21 13:41:26 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: '6937511939:AAGPBxj1d6_n6Ho2Gq6WV-g9KL5Z2OSa2X4',
-	chatId: '-4173503255',
+	botToken: '6910494198:AAEzAiv5SMCwA0MPJAW9JM16y76idqLfFtY',
+	chatId: '-4184265373',
 };
